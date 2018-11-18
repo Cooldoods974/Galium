f10fb5049f5069ad38f69f17938cc7bf46e19dd3
ItsZevix <43051834+ItsZevix@users.noreply.github.com>
2018-11-18 17:33:14 -0500

    Add files via upload

--- Galium/NPCs/NPCdroploot.cs
+++ Galium/NPCs/NPCdroploot.cs
@@ -9,13 +9,12 @@ namespace Galium.NPCs
 {
 	public override void NPCLoot(NPC npc)
 	{
+		// Addtional if statements here if you would like to add drops to other vanilla npc.
 		if(npc.type == NPCID.EyeofCthulhu) //1
 		{
-		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Retina"), 2);
+		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Retina"), 1);
 		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.Lens, 3);
 		}
-
-		// Addtional if statements here if you would like to add drops to other vanilla npc.
 
         if(npc.type == NPCID.GiantTortoise)//2
 		if (Main.rand.Next(10) == 0) //1 in 3 chance
@@ -29,20 +28,25 @@ namespace Galium.NPCs
 		}
 
 		if(npc.type == NPCID.SkeletronHead)//4
-		if (Main.rand.Next(5) == 0) //1 in 3 chance
+		if (Main.rand.Next(2) == 0) //1 in 3 chance
 		{
 		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("LifePiece"), 3);
 		}
-
-		if(npc.type == NPCID.Plantera)//5
+		if(npc.type == NPCID.SkeletronHead)//4
+		if (Main.rand.Next(2) == 0) //1 in 3 chance
 		{
-		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("LifeFruitSeed"), 3);
+		Item.NewItem(npc.getRect(), ItemID.Bone, Main.rand.Next(5, 15));
 		}
 
 		if(npc.type == NPCID.Plantera)//5
 		if (Main.rand.Next(5) == 0) //1 in 3 chance
 		{
 		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.Uzi, 1);
+		}
+		if(npc.type == NPCID.Plantera)//24
+		if(Main.rand.Next(4) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
 		}
 		
 		//angry bones, ph dun enemys
@@ -153,17 +157,33 @@ namespace Galium.NPCs
 		{
 		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.LihzahrdPowerCell);
 		}
+		if(npc.type == NPCID.Lihzahrd)//24
+		if(Main.rand.Next(4) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
 		
 		if(npc.type == NPCID.LihzahrdCrawler)//23
 		if(Main.rand.Next(8) == 0)
 		{
 		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.LihzahrdPowerCell);
 		}
+		if(npc.type == NPCID.LihzahrdCrawler)//23
+		if(Main.rand.Next(4) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
 		
 		if(npc.type == NPCID.FlyingSnake)//24
 		if(Main.rand.Next(8) == 0)
 		{
 		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.LihzahrdPowerCell);
+		}
+
+		if(npc.type == NPCID.FlyingSnake)//24
+		if(Main.rand.Next(4) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
 		}
 
 		if(npc.type == NPCID.Pumpking)//25
@@ -220,10 +240,101 @@ namespace Galium.NPCs
 		{
 		Item.NewItem(npc.getRect(), ItemID.Hay, Main.rand.Next(20, 37));
 		}
+
+		if(npc.type == NPCID.WallofFlesh)//27
+		{
+		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("BallofFlesh"));
+		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.SoulofNight, 15);
+		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.SoulofLight, 15);
+		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.GravitationPotion);
+		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.GoldCoin, 75);
+
+		}
+
+		if(npc.type == NPCID.MoonLordCore)//27
+		{
+		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Glowmite"));
+		}
+		if(npc.type == NPCID.Arapaima)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.AngryTrapper)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.GiantTortoise)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.GiantFlyingFox)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.Derpling)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.JungleCreeper)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.JungleCreeperWall)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.Moth)//24
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		Item.NewItem(npc.getRect(), ItemID.HoneyLamp, Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.BigMossHornet)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.GiantMossHornet)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.LittleMossHornet)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.MossHornet)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+		if(npc.type == NPCID.TinyMossHornet)//24
+		if(Main.rand.Next(3) == 0)
+		{
+		Item.NewItem(npc.getRect(), mod.ItemType("JungleSoul"), Main.rand.Next(1, 3));
+		}
+
+		if(npc.type == NPCID.Frog)//25
+		if(Main.rand.Next(12) == 0)
+		{
+		Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.FrogLeg);
+		}
 	}
 	//buyslots
 	public override void SetupShop(int type, Chest shop, ref int nextSlot)
         {
+			//if (type == NPCID.ArmsDealer && Main.bloodMoon && Main.hardMode && NPC.downedAncientCultist)
+            //{
+            //    shop.item[nextSlot].SetDefaults(mod.ItemType<Items.Weapons.Tier_4.BlightedFang>());
+            //    nextSlot++;
+            //}
 		
 
             if (type == NPCID.ArmsDealer)
@@ -232,7 +343,13 @@ namespace Galium.NPCs
                 shop.item[nextSlot].SetDefaults(mod.ItemType<Items.GunHandBook>());
                 nextSlot++;
 			}
+			if (type == NPCID.ArmsDealer && Main.hardMode)
+            {
+
+				shop.item[nextSlot].SetDefaults(ItemID.GuideVoodooDoll);
+				shop.item[nextSlot].shopCustomPrice = 50000;
+                nextSlot++;
+			}
 		}
-	
-}
+	}
 }
